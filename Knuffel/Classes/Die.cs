da182abfb153badfb48ea9aaf4db39e55ce195a1
BenCamarca <129367588+BenCamarca@users.noreply.github.com>
2023-05-01 18:29:05 +0200

    Further UI Tweaks Further ViewModel Tweaks Edit the classes: Die, Player Started programming the Game Logic

--- Knuffel/Classes/Die.cs
+++ Knuffel/Classes/Die.cs
@@ -11,20 +11,26 @@ namespace Knuffel.Classes
     public class Die
     {
         // Here we create the properties for the die
-        public int Value { get; set; }
+        public int? Value { get; set; }
         public bool Locked { get; set; }
+        public List<Die> DieList { get; set; }
         public Die() { }
         public Die(int value, bool locked)
         {
             Value = value;
             Locked = locked;
         }
-        // Here we create the method for the die
+        // Here we create the method for rolling the die
+        
         public void Roll(Random rnd)
-        {
+        {            
             if (!Locked)
             {
                 Value = rnd.Next(1, 7);
+            }
+            else if(Locked && Value == null)
+            {
+                Value = rnd.Next(1,7);
             }
             else
             {
@@ -32,6 +38,27 @@ namespace Knuffel.Classes
             }
         }
        
+        // Here we create a method for resetting the Locked property of the die to false
+        public void DiceReset()
+        {
+            foreach (Die die in DieList)
+            {
+                die.Locked = false;
+            }
+        }
+        // Here we create a method for changing the Locked property of the die
+        public void ChangeLockStatus(int index)
+        {
+            if (DieList[index].Locked)
+            {
+                DieList[index].Locked = false;
+            }
+            else
+            {
+                DieList[index].Locked = true;
+            }
+        }
+
 
     }
 }
--- Knuffel/Classes/ViewModel.cs
+++ Knuffel/Classes/ViewModel.cs
@@ -18,6 +18,12 @@ namespace Knuffel.Classes
         private string _die5Image;
         private string _rollsLeft;
 
+        // declaring the private variables for the player names
+        private string _player1Name;
+        private string _player2Name;
+        private string _player3Name;
+        private string _player4Name;
+
 
         // declaring the public properties
         public string Die1Image 
@@ -89,6 +95,56 @@ namespace Knuffel.Classes
                 {
                     _rollsLeft = value;
                     OnPropertyChanged(nameof(RollsLeft));
+                }
+            }
+        }
+
+        // declaring the public properties for the player names
+        public string Player1Name
+        {
+            get { return _player1Name; }
+            set
+            {
+                if (_player1Name != value)
+                {
+                    _player1Name = value;
+                    OnPropertyChanged(nameof(Player1Name));
+                }
+            }
+        }
+        public string Player2Name
+        {
+            get { return _player2Name; }
+            set
+            {
+                if (_player2Name != value)
+                {
+                    _player2Name = value;
+                    OnPropertyChanged(nameof(Player2Name));
+                }
+            }
+        }
+        public string Player3Name
+        {
+            get { return _player3Name; }
+            set
+            {
+                if (_player3Name != value)
+                {
+                    _player3Name = value;
+                    OnPropertyChanged(nameof(Player3Name));
+                }
+            }
+        }
+        public string Player4Name
+        {
+            get { return _player4Name; }
+            set
+            {
+                if (_player4Name != value)
+                {
+                    _player4Name = value;
+                    OnPropertyChanged(nameof(Player4Name));
                 }
             }
         }
