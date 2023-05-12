196182b899e40b040df6f3a8c122fdf54e3b94d0
BenCamarca <129367588+BenCamarca@users.noreply.github.com>
2023-05-12 18:16:11 +0200

    Startet nearly from scratch. Changed to MVVM-Pattern

---
+++ Knuffel/Classes/BaseViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knuffel.Classes
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        // declaring the event
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // declaring the method for the event
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (!string.IsNullOrEmpty(propertyName))
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            
+            
+        }
+    }
+}
